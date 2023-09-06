dd5945517c3bff9fd16fb74c98e74cc77c862c53
SAGAR <varshneysamarth2@gmail.com>
2023-09-06 23:29:23 +0530

    added day vise exercise in the recycler view

--- app/src/main/java/com/example/android/varsfit/show_exercise.kt
+++ app/src/main/java/com/example/android/varsfit/show_exercise.kt
@@ -1,13 +1,17 @@
 package com.example.android.varsfit
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.android.varsfit.databinding.ActivityShowExerciseBinding
+import java.time.LocalDate
 
 class show_exercise : AppCompatActivity() {
     private lateinit var binding: ActivityShowExerciseBinding
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityShowExerciseBinding.inflate(layoutInflater)
@@ -17,8 +21,13 @@ class show_exercise : AppCompatActivity() {
         val bundle = receivedIntent.getBundleExtra("bundle")
         val map = bundle!!.getSerializable("mapData") as? Map<String, Any>
 
+        val currentDate = LocalDate.now()
+        val dayOfWeek = currentDate.dayOfWeek.toString()
+
+        val exerciseMap = map!!["exercises"] as Map<String,Any>
+
         binding.exerciseRecyclerView.layoutManager = LinearLayoutManager(this)
-        val adapter = exercise_adapter(this, map!!["exercises"] as Map<String,Any>)
+        val adapter = exercise_adapter(this, exerciseMap!![dayOfWeek] as Map<String,Any>)
         binding.exerciseRecyclerView.adapter = adapter
 
     }
